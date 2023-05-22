befd835d231ade0237b77cb37e29d96d1b5e6798
rm86322 <rm86322@estudante.fieb.edu.br>
2023-05-22 12:32:03 -0300

    aula de OB, objeto, método

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
     public static void main(String[] args) {
         // Aula inicial de orientação à objetos
         Pessoa adao = new Pessoa();
-
+           // Declaração
+        Pessoa qualquer;
+        // Instanciação
+        qualquer = new Pessoa();
+        // Definir forma do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        // Definir comportamento
+        qualquer.falar();
+        System.out.println(qualquer.falar("alto"));
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,5 +2,21 @@ public class Pessoa {
     // Atributo
     String nome;
     String sobrenome;
+    // Métodos
+    public void falar() {
+        System.out.println("falei");
+    }
+    // Sobrecarga do método falar()
+    public String falar(String volume){
+        return "falei" + volume;
 
+    }
+    // Contrutor
+    Pessoa(){
+
+    }
+    Pessoa(String _nome){
+        this.nome = _nome;
+    }
 }
+
